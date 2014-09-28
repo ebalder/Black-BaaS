951345168d8b6151371c6d1fc47502659326a23e
lexas <dislexas@gmail.com>
2014-09-27 18:19:16 -0600

    bind block fields

--- client/basic/main.js
+++ client/basic/main.js
@@ -3,31 +3,40 @@ define(['jquery', 'underscore', 'backbone', 'stickit', 'paginator'], function($,
 
     var DocView = Backbone.View.extend({
         binds: {
-            '[data-doc] [data-field]': {
-                observe: '*'
-                onGet: function($el, attr, model){
+            '[data-field]': {
+                observe: '*',
+                update: function($el, attr, model){
+                    $el = $('[data-field =')
+                }
+            },
+            '.edit[data-field]': {
+                observe: '*',
+                getVal: function($el, attr, model){
                     var field = $el.data('field');
-                    return model.get(field);
+                    var value = $el.html();
+                    model.set(field, value);
                 }
             }
+        },
+        initialize: function($el){
+            var docName = this.docName = $el.data('doc');
+
+            this.doc = bootstrapData[docName];
+            this.model = new DocModel(null, docName);
+        },
+        render: function(opts){
+            $('[data-field]').each(function($el, index){
+                var fieldName = $el.data('field');
+                var selector = '[data-field='+ fieldName +']';
+                this.addBinding(null, selector, fieldName);  
+            });
         }
     });
 
-/* ToDo: instead of binding, building all block views with their models on render */
     var PageView = Backbone.View.extend({
-        binds: {
-            '[data-doc]': {
-                observe: 'docs',
-                update: function($el, attr, model){
-                    var docName = $el.data('doc');
-                    var doc = attr[docName];
-                    var view = this.docViews[doc].render(attr);
-                }
-            }
-        }
         render: function(opts){
             $('[data-doc]').each(function(index, $el){
-                $()
+                var doc = new DocView($el).render();
             });
         }
     });
